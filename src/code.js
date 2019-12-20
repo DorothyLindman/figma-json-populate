89ab53947bcd13a3047965368ebc15d289530d11
DOROTHY LINDMAN <DOROTHY.LINDMAN@HBO.COM>
2019-12-20 09:58:06 -0800

    Revert "updates"
    This reverts commit 436ecf7652595e89ea59eb4875367fc52189c624.

--- src/code.js
+++ src/code.js
@@ -1,101 +1,57 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-// Init empty data
-let data;
-let fills = 0;
-// Options for UI
-const options = {
-    width: 375,
-    height: 300
-};
+var __awaiter =
+	(this && this.__awaiter) ||
+	function(thisArg, _arguments, P, generator) {
+		return new (P || (P = Promise))(function(resolve, reject) {
+			function fulfilled(value) {
+				try {
+					step(generator.next(value));
+				} catch (e) {
+					reject(e);
+				}
+			}
+			function rejected(value) {
+				try {
+					step(generator['throw'](value));
+				} catch (e) {
+					reject(e);
+				}
+			}
+			function step(result) {
+				result.done
+					? resolve(result.value)
+					: new P(function(resolve) {
+							resolve(result.value);
+					  }).then(fulfilled, rejected);
+			}
+			step((generator = generator.apply(thisArg, _arguments || [])).next());
+		});
+	};
 // Start plugin and show the UI
-figma.showUI(__html__, options);
-figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
-    const { type, content } = msg;
-    if (type === 'get-url') {
-        getURL();
-    }
-    if (type === 'set-url') {
-        setURL(msg.content).then(getURL);
-    }
-    // Set data to content of received-data message
-    if (type === 'received-data') {
-        data = msg.content;
-    }
-    // Cancel button is pressed
-    if (type === 'cancel') {
-        figma.closePlugin();
-        return;
-    }
-    // Replace text button is pressed
-    if (type === 'replace-text') {
-        // Create const and assign it the selection
-        const nodes = figma.currentPage.selection;
-        const dataSource = data[content];
-        // Check if anything is selected and if it's a text layer
-        nodes.forEach(function (node) {
-            if (!node) {
-                figma.ui.postMessage('no-selection');
-            }
-            else if (node.type != 'TEXT') {
-                figma.ui.postMessage('non-text');
-                return;
-            }
-            if (msg.sorting === 'random') {
-                // Generate random number
-                const random = genNumber(dataSource.length);
-                // Replace with random value picked from array
-                replaceText(node, dataSource[random]);
-            }
-            else if (msg.sorting === 'original') {
-                // Replace with value based on amount of times filled
-                replaceText(node, dataSource[fills]);
-                if (fills === dataSource.length - 1) {
-                    // Start at beginning of array again if end is reached
-                    fills = 0;
-                }
-                else {
-                    // Add +1 to the amount of times a text node has been filled
-                    fills++;
-                }
-            }
-        });
-    }
-});
-function genNumber(max) {
-    // Generate random number between start of array and last item of array
-    return Math.floor(Math.random() * max);
-}
-// Set URL to ClientStorage
-function setURL(url) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield figma.clientStorage.setAsync('url', url);
-    });
-}
-// Get the URL from ClientStorage
-function getURL() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield figma.clientStorage.getAsync('url').then(str => {
-            if (str != undefined) {
-                figma.ui.postMessage(str);
-                return;
-            }
-            figma.ui.postMessage('no-url');
-        });
-    });
-}
-function replaceText(node, content) {
-    return __awaiter(this, void 0, void 0, function* () {
-        // Get the font of the selected node and load it async
-        yield figma.loadFontAsync(node.fontName);
-        // Set the node it's characters to the message we passed to this function
-        node.characters = String(content);
-    });
+figma.showUI(__html__);
+// Get first selected node
+let node = figma.currentPage.selection[0];
+figma.ui.onmessage = msg =>
+	__awaiter(this, void 0, void 0, function*() {
+		if (msg.type === 'replace-text') {
+			// Create const and assign it the selection
+			const node = figma.currentPage.selection[0];
+			// Check if anything is selected and if it's a text layer
+			if (!node || node.type != 'TEXT') {
+				console.log('No text selected'); // Show error in UI
+				figma.closePlugin();
+			}
+			// Pick value from 'data' sheet
+			// Run replaceText function and pass it the node and replacement text
+			replaceText(node, msg.body);
+		}
+	});
+function replaceText(node, body) {
+	return __awaiter(this, void 0, void 0, function*() {
+		// Get the font of the selected node and load it async
+		yield figma.loadFontAsync(node.fontName);
+		// Set the node it's characters to the message we passed to this function
+		node.characters = String(body);
+		// Close the plugin
+		figma.closePlugin();
+	});
 }
